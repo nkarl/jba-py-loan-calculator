0107939e5a4b04acc5a74464424699219cd71b5a
nkarl <nordenk@pm.me>
2021-07-30 22:32:41 -0700

    stage-2 complete

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -1,12 +1,45 @@
-loan_principal = 'Loan principal: 1000'
-final_output = 'The loan has been repaid!'
-first_month = 'Month 1: repaid 250'
-second_month = 'Month 2: repaid 250'
-third_month = 'Month 3: repaid 500'
 
-# write your code here
-print(loan_principal)
-print(first_month)
-print(second_month)
-print(third_month)
-print(final_output)
+def get_months(principal, monthly_pay):
+    return -(-principal // monthly_pay)
+
+
+def option_m(principal):
+    print("Enter the monthly payment:")
+    monthly = int(input())
+    match monthly:
+        case monthly if monthly == principal:
+            print(f"It will take 1 month to repay the loan")
+        case _:
+            months = get_months(principal, monthly)
+            print(f"It will take {months} months to repay the loan")
+
+
+def get_monthly(principal, num_months):
+    return -(-principal // num_months)
+
+
+def option_p(principal):
+    print("Enter the number of months:")
+    months = int(input())
+    monthly = get_monthly(principal, months)
+    match months:
+        case months if months % 2 == 0:
+            print(f"Your monthly payment = {monthly}")
+        case months if months % 2 != 0:
+            last = principal - (months - 1) * monthly
+            print(f"Your monthly payment = {monthly} and the last payment = {last}")
+    pass
+
+
+print("Enter the loan principal:")
+loan_principal = int(input())
+print("What do you want to calculate?")
+
+print("type \"m\" - for number of monthly payments,")
+print("type \"p\" - for the monthly payment:")
+option = input()
+match option:
+    case "m":
+        option_m(loan_principal)
+    case "p":
+        option_p(loan_principal)
