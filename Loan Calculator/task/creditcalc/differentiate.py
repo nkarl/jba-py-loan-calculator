706e7531787d8d6dcb0375249c7700e76e3de3d8
nkarl <nordenk@pm.me>
2021-07-31 21:39:05 -0700

    updated, passed up to test # 7

--- Loan Calculator/task/creditcalc/differentiate.py
+++ Loan Calculator/task/creditcalc/differentiate.py
@@ -13,12 +13,14 @@ class Diff:
 
     @staticmethod
     def calculator(principal, periods, interest):
+        P = principal
         i = Diff.nominal(interest)
         n = periods
         total = 0
         for m in range(1, n+1):
-            d = int(math.ceil(principal // n + i * (principal - (principal * (m - 1)) // 10)))
-            print(f"Month {m}: payment is {int(math.ceil(d))}")  # \t\t principal={principal}, i={i}, n={n}")
+            d = int(math.ceil(P / n + i * (P - (P * (m - 1) / 10))))
+            # d = math.ceil(principal / n) + math.ceil(i * (principal - (principal * (m - 1)) / 10))
+            print(f"Month {m}: payment is {int(d)}")  # \t\t principal={principal}, i={i}, n={n}")
             total += d
 
         print(f"Overpayment = {total - principal}")
